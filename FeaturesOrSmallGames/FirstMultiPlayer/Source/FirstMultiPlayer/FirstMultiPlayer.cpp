48cf318d9828d96add6e11d45db4c7c98784aaf4
Soohwan123 <90574797+Soohwan123@users.noreply.github.com>
2022-03-26 12:16:41 -0400

    Add files via upload
    Start a new game and implement the files you need. the file is too large to be uploaded

---
+++ FeaturesOrSmallGames/FirstMultiPlayer/Source/FirstMultiPlayer/FirstMultiPlayer.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "FirstMultiPlayer.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, FirstMultiPlayer, "FirstMultiPlayer" );
+ 
---
+++ FeaturesOrSmallGames/FirstMultiPlayer/Source/FirstMultiPlayer/FirstMultiPlayerGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FirstMultiPlayerGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AFirstMultiPlayerGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AFirstMultiPlayerGameMode();
+};
+
+
+
